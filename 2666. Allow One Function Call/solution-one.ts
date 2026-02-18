3fc2cd04654d90171de44120bce942b85a4034ea
Rajdip-Pal <rajdippal.rp@outlook.com>
2026-02-19 03:28:24 +0530

    .

---
+++ 2666. Allow One Function Call/solution-one.ts
@@ -0,0 +1,22 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type OnceFn = (...args: JSONValue[]) => JSONValue | undefined
+
+function once(fn: Function): OnceFn {
+    let isCalled: boolean = false
+
+    return function (this: any, ...args) {
+        if (!isCalled) {
+            isCalled = true
+            return fn.apply(this, args)
+        }
+        return undefined
+    };
+}
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
