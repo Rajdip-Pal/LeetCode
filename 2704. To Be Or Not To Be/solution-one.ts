907d50ed7dbbd7068bac72470e3e66ecb224d91d
Rajdip-Pal <rajdippal.rp@outlook.com>
2026-03-18 22:41:55 +0530

    .

---
+++ 2704. To Be Or Not To Be/solution-one.ts
@@ -0,0 +1,17 @@
+type ToBeOrNotToBe = {
+    toBe: (val: any) => boolean;
+    notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+    return {
+        toBe: (v) => {
+            if (val === v) return true
+            throw new Error("Not Equal")
+        },
+        notToBe: (v) => {
+            if (val !== v) return true
+            throw new Error("Equal")
+        }
+    }
+};
