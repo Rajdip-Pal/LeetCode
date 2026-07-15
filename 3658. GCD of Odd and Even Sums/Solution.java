067706b2c2e5d2fdb8a10b47ba1069b774cf4281
Rajdip-Pal <rajdippal.rp@outlook.com>
2026-07-16 00:06:53 +0530

    .

---
+++ 3658. GCD of Odd and Even Sums/Solution.java
@@ -0,0 +1,12 @@
+
+/**
+ *  Sum of first n even number => Sum(2k)   where k = 1 to k = n => n(n-1)
+ *  Sum of first n odd  number => Sum(2k-i) where k = 1 to k = n => n(n-1) - n
+ */
+
+class Solution {
+
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
